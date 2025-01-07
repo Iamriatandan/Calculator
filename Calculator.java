8eaa782c8dbba02a1de9be7a4afa1742349e18ea
iamriatandan <tandanria@gmail.com>
2025-01-07 11:03:25 +0530

    Secomd commit

--- Calculator.java
+++ Calculator.java
@@ -6,5 +6,12 @@ public class Calculator{
         int b = sc.nextInt();
         int result = a+b;
         System.out.println(result);
+
+        int subtraction = a-b;
+        int multiplication = a*b;
+        int division = a/b;
+        System.out.println(subtraction);
+        System.out.println(multiplication);
+        System.out.println(division);
     }
 }
